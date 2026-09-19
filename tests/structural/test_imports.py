66a8099bdaa1f32c03651dc492f2fc386bc4fa0b
Eran <agmon.eran@gmail.com>
2026-09-19 16:56:55 -0400

    feat(structural): scaffold viva_mgen.structural + import pbg_parsimony

---
+++ tests/structural/test_imports.py
@@ -0,0 +1,7 @@
+def test_pbg_parsimony_importable():
+    import pbg_parsimony
+    from pbg_parsimony import Ingredient, Capsule, Chromosome, build_pack  # noqa
+
+
+def test_structural_package_importable():
+    import viva_mgen.structural  # noqa
---
+++ viva_mgen/structural/__init__.py
@@ -0,0 +1,1 @@
+"""Bridge from viva_mgen's whole-cell model to the pbg_parsimony 3D packing engine."""
